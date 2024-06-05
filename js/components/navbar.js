470c2750566c2d5c118e2e1da9b7c0c281154a67
TheSillyCoder <sarkardebayan45@gmail.com>
2024-06-05 13:46:32 +0530

    created navbar component

---
+++ js/components/navbar.js
@@ -0,0 +1,28 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super()
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="nav-prof">
+            <img src="/images/prof.jpg" alt="dplol">
+            <h3>Debayan Sarkar</h3>
+            <p><a href="https://github.com/TheSillyCoder" target="_blank" class="intext-link">@TheSillyCoder</a></p>
+            <p>IISER Kolkata</p>
+        </div>
+        <ul class="nav-list" >
+            <li id="home" class="nav-item"><a href="/" class="nav-link">Home</a></li>
+            <li id="notes" class="nav-item"><a href="/notes.html" class="nav-link">Notes</a></li>
+            <li id="software" class="nav-item"><a href="/software.html" class="nav-link">Software</a></li>
+            <li id="blog" class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
+        </ul>
+        <div class="socials">
+            <a href="https://github.com/TheSillyCoder" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="mailto:ds22ms002@iiserkol.ac.in"><i class="fa fa-envelope"></i></a>
+            <a href="https://www.last.fm/user/sillycoder" target="_blank"><i class="fa-brands fa-lastfm"></i></a>
+        </div>
+        `
+    }
+}
+
+customElements.define('navbar-component', Navbar)
--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const navbar = document.querySelector(".nav")
+const active = navbar.getAttribute("data-active")
+const activeTab = document.getElementById(active)
+activeTab.classList.add("active")
 const navToggleBtn = document.querySelector(".nav-toggle")
 
 navToggleBtn.addEventListener("click", () => {
     const visible = navbar.getAttribute("data-visible")
-    if (visible === "false"){
+    if (visible === "false") {
         navbar.setAttribute("data-visible", true)
         navToggleBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>"
-    } else if (visible === "true"){
+    } else if (visible === "true") {
         navbar.setAttribute("data-visible", false)
         navToggleBtn.innerHTML = '<i class="fas fa-bars"></i>'
     }
