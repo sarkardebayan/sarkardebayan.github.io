4c2eb1634c46f3ed5ee404a0275f9bd863ce7718
TheSillyCoder <sarkardebayan45@gmail.com>
2024-12-22 21:53:52 +0530

    added a music page

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -12,13 +12,15 @@ class Navbar extends HTMLElement {
         </div>
         <ul class="nav-list" >
             <li id="home" class="nav-item"><a href="/" class="nav-link">Home</a></li>
-            <li id="notes" class="nav-item"><a href="/notes.html" class="nav-link">Notes</a></li>
             <li id="software" class="nav-item"><a href="/software.html" class="nav-link">Software</a></li>
+            <li id="music" class="nav-item"><a href="/music.html" class="nav-link">Music</a></li>
+            <li id="notes" class="nav-item"><a href="/notes.html" class="nav-link">Notes</a></li>
             <li id="blog" class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
         </ul>
         <div class="socials">
             <a href="https://github.com/TheSillyCoder" target="_blank"><i class="fa-brands fa-github"></i></a>
             <a href="mailto:ds22ms002@iiserkol.ac.in"><i class="fa fa-envelope"></i></a>
+            <a href="" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
             <a href="https://www.last.fm/user/sillycoder" target="_blank"><i class="fa-brands fa-lastfm"></i></a>
         </div>
         `
